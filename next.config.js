693ef34ec12e5de1aa68e080ec33a75d2e601fdc
jake-harris-prooduct <jakeharrisproduct@gmail.com>
2024-10-30 17:15:24 +1300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['drive.google.com'], // Add any other domains you'll load images from
+  },
+};
