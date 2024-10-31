f900e509c011cf59113308e8b9f7c1d173930fd8
jake-harris-prooduct <jakeharrisproduct@gmail.com>
2024-10-31 17:21:58 +1300

    Update utils.ts
    Added Google drive image finder

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,13 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// lib/utils.ts
+// Add this alongside your other utility functions
+
+export const getGoogleDriveImageUrl = (fileId: string): string => {
+  if (!fileId) return "/api/placeholder/400/400"
+  return `https://drive.google.com/uc?export=view&id=${fileId}`
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
